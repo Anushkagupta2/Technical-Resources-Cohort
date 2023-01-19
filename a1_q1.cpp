8dc1f674a06402a8549534bb53ca0f02ba423a16
Anushkagupta2 <120239202+Anushkagupta2@users.noreply.github.com>
2023-01-20 00:12:57 +0530

    Submitting Q1 of Assignment1

---
+++ a1_q1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char i;
+    for(i=1;i<=5;i++)
+        cout<<'*';
+    cout<<endl<<"   *"<<endl;
+    cout<<"  *"<<endl;
+    cout<<" *"<<endl;
+    for(i=1;i<=5;i++)
+        cout<<'*';
+    return 0;
+}
